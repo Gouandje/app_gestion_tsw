5570672604d4b51354b5fa4e60eb39cde8f215b9
unknown <gouandje@gmail.com>
2024-03-08 11:11:15 +0100

    mise a jour db service

--- src/app.module.ts
+++ src/app.module.ts
@@ -46,17 +46,17 @@ import { DelecountryModule } from './delecountry/delecountry.module';
       load: configs,
       isGlobal: true,
     }),
-    //   MongooseModule.forRoot(
-    //     // `mongodb://mongo:bhHE2GE2FBG-cb-CH2-16fGg5b4C3Hbf@monorail.proxy.rlwy.net:24357`,32188
+      MongooseModule.forRoot(
+        // `mongodb://mongo:bhHE2GE2FBG-cb-CH2-16fGg5b4C3Hbf@monorail.proxy.rlwy.net:24357`,32188
         
-    //     // `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}.ftyqrzd.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
-    //   `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASEPASSWORD}@${process.env.DATABASEHOST}:32188/${process.env.MONGODB_DATABASE}?authSource=admin`,
-    //  ),
-    MongooseModule.forRootAsync({
-    inject: [DatabaseService],
-    imports: [DatabaseModule],
-    useFactory: (databaseService: DatabaseService) => databaseService.createMongooseOptions(),
-    }),
+        // `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}.ftyqrzd.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
+      `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASEPASSWORD}@${process.env.DATABASEHOST}:32188/${process.env.MONGODB_DATABASE}?authSource=admin`,
+     ),
+    // MongooseModule.forRootAsync({
+    // inject: [DatabaseService],
+    // imports: [DatabaseModule],
+    // useFactory: (databaseService: DatabaseService) => databaseService.createMongooseOptions(),
+    // }),
     UserModule,
     ManagerModule, 
     ProduitModule, 
