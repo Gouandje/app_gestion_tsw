737351a2702ea9517d9a45391b123b27620e0ed2
unknown <gouandje@gmail.com>
2024-03-08 17:21:57 +0100

    mise a jour info managers correction

--- src/manager/manager.controller.ts
+++ src/manager/manager.controller.ts
@@ -3,6 +3,7 @@ import { ManagerService } from './manager.service';
 import { CreateManagerDto } from './dto/create-manager.dto';
 import { UpdateManagerDto } from './dto/update-manager.dto';
 import { UpdateStatusDto } from './dto/update-status.dto';
+import { InfoManagerDto } from './dto/info_manager.dto';
 
 @Controller('manager')
 export class ManagerController {
@@ -13,14 +14,19 @@ export class ManagerController {
     return this.managerService.create(createManagerDto);
   }
 
-  @Get('ajoutmanager')
-  createDirect() {
-    return this.managerService.createDirect();
-  }
+  // @Get('ajoutmanager')
+  // createDirect() {
+  //   return this.managerService.createDirect();
+  // }
 
   @Get('allmanager')
   findAll() {
     return this.managerService.findAll();
+  }
+
+  @Post('infomanager')
+  getinmanager(@Body() infomanagerDto: InfoManagerDto){
+    return this.managerService.findAllMnager(infomanagerDto);
   }
 
   @Get('allmanagerSupervisor')
