f546f14ef545e5cc9b9fb6da3608fb935e12a1ba
unknown <gouandje@gmail.com>
2024-03-09 12:47:06 +0100

    le backup code mis a jour module backup

--- src/backup/backup.module.ts
+++ src/backup/backup.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BackupService } from './backup.service';
 import { BackupController } from './backup.controller';
+import { MailService } from './mail.service';
+import { BackupSchedulerService } from './backup-scheduler.service';
 
 @Module({
   controllers: [BackupController],
-  providers: [BackupService]
+  providers: [BackupService,MailService,BackupSchedulerService],
+  exports: [BackupService,MailService,BackupSchedulerService]
+
 })
 export class BackupModule {}
