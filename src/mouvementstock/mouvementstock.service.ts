7f92a460f08c2f4a6b93aed049e889aed9dc1721
unknown <gouandje@gmail.com>
2024-03-09 01:17:09 +0100

    mvt en insertion direct

--- src/mouvementstock/mouvementstock.service.ts
+++ src/mouvementstock/mouvementstock.service.ts
@@ -25,134 +25,134 @@ export class MouvementstockService {
     private agenceService: AgenceService
   ){}
 
-  async createDirect(){
-    const data = {
-        "bureauId":"65eaa175382bb54abbf6ce63",
-        "date_sortie":" 2024-03-09",
-        "items":[
-            {
-            "productId": "65eac117894f343b90dd4f52",
-            "quantity": 45
-            },
-            {
-            "productId": "65eac117894f343b90dd4f54",
-            "quantity": 44
-            },
-            {
-            "productId": "65eac117894f343b90dd4f56",
-            "quantity": 0
-            },
-            {
-            "productId": "65eac117894f343b90dd4f58",
-            "quantity": 43
-            },
-            {
-            "productId": "65eac117894f343b90dd4f5a",
-            "quantity": 0
-            },
-            {
-            "productId": "65eac117894f343b90dd4f5c",
-            "quantity": 0
-            },
-            {
-            "productId": "65eac117894f343b90dd4f5e",
-            "quantity": 160
-            },
-            {
-            "productId": "65eac117894f343b90dd4f60",
-            "quantity": 208
-            },
-            {
-            "productId": "65eac117894f343b90dd4f62",
-            "quantity": 250
-            },
-            {
-            "productId": "65eac117894f343b90dd4f64",
-            "quantity": 15
-            },
-            {
-            "productId": "65eac117894f343b90dd4f66",
-            "quantity": 0
-            },
-            {
-            "productId": "65eac117894f343b90dd4f68",
-            "quantity": 47
-            },
-            {
-            "productId": "65eac117894f343b90dd4f6a",
-            "quantity": 71
-            },
-            {
-            "productId": "65eac117894f343b90dd4f6c",
-            "quantity": 49
-            },
-            {
-            "productId": "65eac117894f343b90dd4f6e",
-            "quantity": 45
-            },
-            {
-            "productId": "65eac117894f343b90dd4f70",
-            "quantity": 64
-            },
-            {
-            "productId": "65eac117894f343b90dd4f72",
-            "quantity": 0
-            },
-            {
-            "productId": "65eac117894f343b90dd4f74",
-            "quantity": 49
-            },
-            {
-            "productId": "65eac117894f343b90dd4f76",
-            "quantity": 59
-            },
-            {
-            "productId": "65eac117894f343b90dd4f78",
-            "quantity": 66
-            },
-            {
-            "productId": "65eb9fb36410b3da421e46a0",
-            "quantity": 45
-            },
-            {
-            "productId": "65eac117894f343b90dd4f7a",
-            "quantity": 38
-            },
-            {
-            "productId": "65eac117894f343b90dd4f7c",
-            "quantity": 54
-            }
-        ]
-      };
-    for(let i = 0; i<data.items.length; i++){
+  async createDirect(createMouvementstockDto: CreateMouvementstockDto){
+    // const data = {
+    //     "bureauId":"65eaa175382bb54abbf6ce63",
+    //     "date_sortie":" 2024-03-09",
+    //     "items":[
+    //         {
+    //         "productId": "65eac117894f343b90dd4f52",
+    //         "quantity": 45
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f54",
+    //         "quantity": 44
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f56",
+    //         "quantity": 0
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f58",
+    //         "quantity": 43
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f5a",
+    //         "quantity": 0
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f5c",
+    //         "quantity": 0
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f5e",
+    //         "quantity": 160
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f60",
+    //         "quantity": 208
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f62",
+    //         "quantity": 250
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f64",
+    //         "quantity": 15
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f66",
+    //         "quantity": 0
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f68",
+    //         "quantity": 47
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f6a",
+    //         "quantity": 71
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f6c",
+    //         "quantity": 49
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f6e",
+    //         "quantity": 45
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f70",
+    //         "quantity": 64
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f72",
+    //         "quantity": 0
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f74",
+    //         "quantity": 49
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f76",
+    //         "quantity": 59
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f78",
+    //         "quantity": 66
+    //         },
+    //         {
+    //         "productId": "65eb9fb36410b3da421e46a0",
+    //         "quantity": 45
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f7a",
+    //         "quantity": 38
+    //         },
+    //         {
+    //         "productId": "65eac117894f343b90dd4f7c",
+    //         "quantity": 54
+    //         }
+    //     ]
+    //   };
+    for(let i = 0; i<createMouvementstockDto.items.length; i++){
       const mvtstock = {
-        bureauId: data.bureauId,
-        date_sortie: data.date_sortie,
-        productId: data.items[i].productId,
-        quantity: data.items[i].quantity,
+        bureauId: createMouvementstockDto.bureauId,
+        date_sortie: createMouvementstockDto.date_sortie,
+        productId: createMouvementstockDto.items[i].productId,
+        quantity: createMouvementstockDto.items[i].quantity,
       };
 
       const createdStock = await this.mvtstockModel.create(mvtstock);
       if(createdStock){
-        const bureau = await this.agenceService.findSingleAgengence(data.bureauId);
+        const bureau = await this.agenceService.findSingleAgengence(createMouvementstockDto.bureauId);
         console.log(bureau);
-        const product = await this.agenceStockService.findagenceproduit(data.bureauId, data.items[i].productId);
+        const product = await this.agenceStockService.findagenceproduit(createMouvementstockDto.bureauId, createMouvementstockDto.items[i].productId);
         if(product == null){
           const createStockagenceDto: CreateStockagenceDto = {
-            agenceId: data.bureauId,
-            productId: data.items[i].productId,
-            quantity: data.items[i].quantity,
-            quantitytotalenmagasin: data.items[i].quantity
+            agenceId: createMouvementstockDto.bureauId,
+            productId: createMouvementstockDto.items[i].productId,
+            quantity: createMouvementstockDto.items[i].quantity,
+            quantitytotalenmagasin: createMouvementstockDto.items[i].quantity
           };
           const result = await this.agenceStockService.create(createStockagenceDto);
          
         }else{
           
           const updateStockagenceDto: UpdateStockagenceDto = {
-            agenceId: data.bureauId,
-            productId: data.items[i].productId,
-            quantity: product.quantity + data.items[i].quantity,
-            quantitytotalenmagasin: product.quantity + data.items[i].quantity,
+            agenceId: createMouvementstockDto.bureauId,
+            productId: createMouvementstockDto.items[i].productId,
+            quantity: product.quantity + createMouvementstockDto.items[i].quantity,
+            quantitytotalenmagasin: product.quantity + createMouvementstockDto.items[i].quantity,
           };
 
           const result = await this.agenceStockService.updateagenceStock(product._id.toString('hex'),  updateStockagenceDto);
