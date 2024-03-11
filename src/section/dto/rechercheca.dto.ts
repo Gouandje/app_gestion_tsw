ebe4cfc271c199b01c40ad7c6d2b4b853e45659d
unknown <gouandje@gmail.com>
2024-03-11 03:19:59 +0100

    modifiee

--- src/section/dto/rechercheca.dto.ts
+++ src/section/dto/rechercheca.dto.ts
@@ -2,10 +2,7 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class RecherchecaDto {
-    @IsString()
     param1: string;
-
-    @IsString()
     param2: string;
 
 }
