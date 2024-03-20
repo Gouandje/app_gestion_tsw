a500462f91076cee727710ffbc93dc6ed2201413
unknown <gouandje@gmail.com>
2024-03-20 05:03:40 +0100

    modif compare1

--- src/weekendy/weekendy.service.ts
+++ src/weekendy/weekendy.service.ts
@@ -53,6 +53,8 @@ export class WeekendyService {
     for(let i=0; i<createWeekendyDto.items.length; i++){
 
       const product = await this.stockagenceService.findagenceproduit(createWeekendyDto.bureauId, createWeekendyDto.items[i].productId);
+      console.log(createWeekendyDto.items[i].quantity - product.quantity);
+      const value = createWeekendyDto.items[i].quantity - product.quantity;
       if(createWeekendyDto.items[i].quantity <= product.quantity){
         weekendproduct.push(createWeekendyDto.items[i]);
       }else{
